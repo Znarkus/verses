c4eb5060337dcea32947049bc27ab96e7a516f79
Josef Lekardal <josef@workbuster.se>
2018-04-09 07:20:43 +0200

    removes old active styles

--- src/client/components/Item.js
+++ src/client/components/Item.js
@@ -1,17 +1,11 @@
 import React from 'react'
 
-const Item = ({ item, outputItem, currentSlide }) => {
-  const style = {
-    backgroundColor: currentSlide === item.id && 'grey'
-  }
-
-  return (
-    <li
-      onClick={() => outputItem(item)}
-      className={currentSlide === item.id ? 'active' : undefined}>
-      {item.content}
-    </li>
-  )
-}
+const Item = ({ item, outputItem, currentSlide }) => (
+  <li
+    onClick={() => outputItem(item)}
+    className={currentSlide === item.id ? 'active' : undefined}>
+    {item.content}
+  </li>
+)
 
 export default Item
